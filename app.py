cf1f355b519e5eff022e9cca111627203f044b4e
Susann Vorberg <susann.vorberg@gmail.com>
2018-01-05 15:25:06 +0100

    changed app settings

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+# Import required libraries
+import os
+from random import randint
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+import flask
 import dash
+from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash(__name__)
-server = app.server
 
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+
+# Put your Dash code here
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -20,7 +36,7 @@ app.layout = html.Div([
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 
+
+# Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
+    app.server.run(debug=True, threaded=True)
